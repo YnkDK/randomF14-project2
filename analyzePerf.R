b613bc319ba18862d3690ed8c9097fd5f667bc7d
YnkDK <ms@ynk.gl>
2014-05-22 16:56:22 +0200

    Counting 'Yes' responses for each data set

---
+++ analyzePerf.R
@@ -0,0 +1,38 @@
+library(lattice)
+library(xtable)
+# Read and format our performance 
+D <- read.table("performance.out", fill = TRUE)
+colnames(D) <- c("Program", "dataA", "dataB", "seed", "time", "response")
+
+# Convert from nanoseconds to milliseconds
+D$time <- D$time/1000000.0
+D$n <- 10^as.numeric(gsub("\\D", "", D$dataA))
+D$Program <- factor(D$Program)
+
+# Set output file
+pdf("graphs.pdf", width=11.7, height=8.3)
+par(mar=c(5,3,2,2)+0.1)
+
+# Make sure all points are unique
+trellis.par.set(superpose.symbol=list(pch = 1:length(unique(D$Program))))
+dataPlot <- xyplot(time ~ n,
+                   groups = Program,
+                   data = D,
+                   type = c("a", "p"),
+                   scales = list(x = list(log = 10),
+                                 y = list(log = FALSE)),
+                   xlab = "n", 
+                   ylab = "Time (ms)",
+                   auto.key = TRUE)
+
+print(dataPlot, width=200)
+# Close the dev
+silence = dev.off()
+
+D$response <- ifelse(D$response == "Yes", 1, 0)
+
+s <- aggregate(response ~ Program + n, data = D, FUN = 'sum')
+s <- s[order(s$Program, s$n),]
+table <- xtable(s, digits = 0)
+
+print(table, type= "latex", include.rownames=FALSE)
