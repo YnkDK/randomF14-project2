018ffea018527e86613b13161c44e728398471e7
YnkDK <ms@ynk.gl>
2014-05-28 08:33:48 +0200

    diff stops at first difference

--- sortAndDiff.sh
+++ sortAndDiff.sh
@@ -2,7 +2,7 @@
 ## Sorts both files given as argument and then compares them
 ## Usage:	bash -f sortAndDiff.sh data-file-a data-file-b
 
-DIFF=$(diff <(sort "$1") <(sort "$2"))
+DIFF=$(diff --brief --minimal <(sort "$1") <(sort "$2"))
 
 if [ "$DIFF" == "" ]; then
 	echo "Yes"
